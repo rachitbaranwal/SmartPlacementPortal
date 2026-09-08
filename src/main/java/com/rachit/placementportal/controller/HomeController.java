1624eadda05851d3c61274c6df00527b9a07d2bc
rachitbaranwal <rachitb3104@gmail.com>
2026-09-08 10:15:34 +0530

    Fix home page mapping

--- src/main/java/com/rachit/placementportal/controller/HomeController.java
+++ src/main/java/com/rachit/placementportal/controller/HomeController.java
@@ -1,24 +1,13 @@
 package com.rachit.placementportal.controller;
 
-import com.rachit.placementportal.service.PlacementDriveService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    private final PlacementDriveService placementDriveService;
-
-    public HomeController(PlacementDriveService placementDriveService) {
-        this.placementDriveService = placementDriveService;
-    }
-
     @GetMapping("/")
-    public String home(Model model) {
-
-        model.addAttribute("drives", placementDriveService.getAllDrives());
-
+    public String home() {
         return "index";
     }
 }
