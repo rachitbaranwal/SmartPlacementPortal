e5c62d4c1ddaab8a3ea138d03cdfd3357dd4b49b
rachitbaranwal <rachitb3104@gmail.com>
2026-09-08 10:10:09 +0530

    Fix JSP Railway deployment

--- src/main/java/com/rachit/placementportal/PlacementportalApplication.java
+++ src/main/java/com/rachit/placementportal/PlacementportalApplication.java
@@ -2,12 +2,18 @@ package com.rachit.placementportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PlacementportalApplication {
+public class PlacementportalApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PlacementportalApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PlacementportalApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(PlacementportalApplication.class, args);
+    }
 }
